c21bd108c3439a1a510b191229bb3a01b69d15fc
mr.mitch <mr.mitch@vfemail.net>
2011-06-20 19:26:33 +0200

    corrected formatting

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-php build/build.php src\iso690.hamlbuild\xmllint\xmllint --format src\iso690.csl -o src\iso690.csl
+php build/build.php src\iso690.haml
+build\xmllint\xmllint --format src\iso690.csl -o src\iso690.csl
 pandoc test\test.markdown --bibliography test\biblatex2.bbx --csl src\iso690.csl > test\test.html
 start test\test.html
--- test.bat
+++
@@ -1,3 +0,0 @@
-php build/build.php src\iso690.haml
-pandoc test\test.markdown --bibliography test\biblatex2.bbx --csl src\iso690.csl > test\test.html
-start test\test.html
